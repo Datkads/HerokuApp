ade9b1e3ea135b2a2d45c767e92444c33dc23b31
David <myerszbm@gmail.com>
2023-11-04 13:10:37 +0400

    TMS-1 lesson 5 Add Remove test completed

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class AddRemoveElementsTest {
